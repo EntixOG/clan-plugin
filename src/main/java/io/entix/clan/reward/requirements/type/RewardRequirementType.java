dda427cf4ee1f753d34c80b98e23fcdfb2c2dbc7
EntixOG <zittrich28@gmail.com>
2024-10-08 20:12:12 +0200

    Add custom requirement for rewards

---
+++ src/main/java/io/entix/clan/reward/requirements/type/RewardRequirementType.java
@@ -0,0 +1,33 @@
+package io.entix.clan.reward.requirements.type;
+
+import io.entix.clan.reward.requirements.AbstractRewardRequirement;
+import io.entix.clan.reward.requirements.impl.MobRewardRequirement;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+import org.bukkit.Material;
+import org.jetbrains.annotations.Nullable;
+
+@Getter
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public enum RewardRequirementType {
+
+    MONSTER_KILL("PvE (Player vs Entity)", Material.DIAMOND_SWORD, MobRewardRequirement.class),
+    PLAYER_KILL("PvP (Player vs Player)", Material.WOODEN_SWORD, MobRewardRequirement.class);
+
+    String requirementName;
+    Material requirementIcon;
+    Class<? extends AbstractRewardRequirement> rewardRequirement;
+
+    public static @Nullable RewardRequirementType findRewardByName(@NonNull String typeName) {
+        for (RewardRequirementType requirementType : values()) {
+            if (typeName.equalsIgnoreCase(requirementType.name())) {
+                return requirementType;
+            }
+        }
+        return null;
+    }
+}
