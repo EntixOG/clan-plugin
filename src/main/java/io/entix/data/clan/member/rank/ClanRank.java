5398a3d077e1311d48d1757197c41e0c113fe55e
entixog <zittrich28@gmail.com>
2024-10-10 18:06:10 +0200

    Add permissions to rank and a methode to check permission

--- src/main/java/io/entix/data/clan/member/rank/ClanRank.java
+++ src/main/java/io/entix/data/clan/member/rank/ClanRank.java
@@ -1,20 +1,31 @@
 package io.entix.data.clan.member.rank;
 
+import io.entix.data.clan.member.permission.ClanPermission;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.experimental.FieldDefaults;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public enum ClanRank {
 
-    MEMBER("Mitglied"),
-    MOD("Moderator"),
-    ADMIN("Admin"),
-    OWNER("Besitzer");
+    MEMBER("Mitglied", Collections.emptyList()),
+    MOD("Moderator", Collections.emptyList()),
+    ADMIN("Admin", Collections.emptyList()),
+    OWNER("Besitzer", List.of(ClanPermission.values()));
 
     String rankName;
+    List<ClanPermission> allowedPermission;
+
+    public boolean hasPermission(@NonNull ClanPermission permission) {
+        return allowedPermission.contains(permission);
+    }
 
 }
